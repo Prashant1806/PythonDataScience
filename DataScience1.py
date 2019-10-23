e93f0993ae134a5ddeede0ef8c29a7596a5e6564
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2019-10-23 18:59:39 +0530

    PC

---
+++ DataScience1.py
@@ -0,0 +1,18 @@
+#Storing DataFrame in CSV Format
+
+import pandas as pd
+
+# assigning three series to s1, s2, s3
+s1 = pd.Series([0, 4, 8])
+s2 = pd.Series([1, 5, 9])
+s3 = pd.Series([2, 6, 10])
+
+# taking index and column values
+dframe = pd.DataFrame([s1, s2, s3])
+
+# assign column name
+dframe.columns =['Prashant', 'Data', 'Science']
+
+# write data to csv file
+dframe.to_csv('PrashantDataScience.csv', index = False)
+dframe.to_csv('PrashantDataScience.csv', index = True)
